a7bd3133344b67fbb6abdcb006cbf3d2fabbc5a5
ramanthatte26 <ramanthatte261@gmail.com>
2025-08-05 10:00:38 +0530

    Initial commit

---
+++ src/test/java/com/cargopro/CargoproBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cargopro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CargoproBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
